d9c623d39a84f4133eaf7f83426b5aef0bac98fc
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-24 08:45:34 +0530

    376. Setting Up a Post Model with mongoose

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -17,4 +18,9 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose.connect(
+    "mongodb+srv://abhi:ninja123@cluster0.klypvcs.mongodb.net/messages?retryWrites=true&w=majority"
+)
+.then(result => {
+    app.listen(8080);
+})
