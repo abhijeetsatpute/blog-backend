562be2cf6a067e795771e8dfad5a42b81d2ab107
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-24 08:15:23 +0530

    374. Adding a Create Post Endpoint

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,16 +2,16 @@ exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
       {
-        _id: '1',
-        title: 'First Post',
-        content: 'This is the first post!',
-        imageUrl: 'images/duck.png',
+        _id: "1",
+        title: "First Post",
+        content: "This is the first post!",
+        imageUrl: "images/duck.png",
         creator: {
-          name: 'Maximilian'
+          name: "Maximilian",
         },
-        createdAt: new Date()
-      }
-    ]
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
@@ -20,7 +20,15 @@ exports.createPost = (req, res, next) => {
   const content = req.body.content;
   // Create post in db
   res.status(201).json({
-    message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
+    message: "Post created successfully!",
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: {
+        name: "Maximilian",
+      },
+      createdAt: new Date(),
+    },
   });
 };
