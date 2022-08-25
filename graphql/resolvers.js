e9751411aa40c3e9160283c7e3ef327575a02cb3
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-25 21:07:21 +0530

    422-3. Defining Mutation Schema | Adding a Mutation Resolver & GraphiQL
    + graphiql: true for GUI get /graphql

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -3,10 +3,24 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 module.exports = {
-    hello(){
-        return {
-            text: "Hello world",
-            views : 154
-        }
+  createUser: async function({ userInput }, req) {
+    //   const email = args.userInput.email;
+    const existingUser = await User.findOne({ email: userInput.email });
+    if (existingUser) {
+      const error = new Error('User exists already!');
+      throw error;
     }
+    const hashedPw = await bcrypt.hash(userInput.password, 12);
+    const user = new User({
+      email: userInput.email,
+      name: userInput.name,
+      password: hashedPw
+    });
+    const createdUser = await user.save();
+    return { ...createdUser._doc, _id: createdUser._id.toString() };
+  },
+
+  hello() {
+    return "Hello World!";
+  }
 };
