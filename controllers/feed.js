d8a837de492a8a66e383e2047947e26657ba7327
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-25 07:38:19 +0530

    395. Clearing Post-User Relations : using .pull() Mongoose method to clear relations

--- controllers/feed.js
+++ controllers/feed.js
@@ -169,6 +169,13 @@ exports.deletePost = (req, res, next) => {
       clearImage(post.imageUrl);
       return Post.findByIdAndRemove(postId);
     })
+    .then(result => {
+      return User.findById(req.userId);
+    })
+    .then(user => {
+      user.posts.pull(postId);
+      return user.save();
+    })
     .then((result) => {
       console.log(result);
       res.status(200).json({ message: "Deleted post." });
