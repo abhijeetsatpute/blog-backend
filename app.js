ad6b988537f6e6529075788bd3edf7acef5c6d2f
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-26 12:45:34 +0530

    440. Deleting Posts

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const fs = require('fs');
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -11,6 +10,7 @@ const {graphqlHTTP } = require('express-graphql');
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
 const auth = require('./middleware/auth');
+const { clearImage } = require('./util/file');
 
 const app = express();
 
@@ -106,8 +106,3 @@ mongoose
   .then((result) => {
     app.listen(8080);
   });
-
-const clearImage = filePath => {
-  filePath = path.join(__dirname, '..', filePath);
-  fs.unlink(filePath, err => console.log(err));
-};
