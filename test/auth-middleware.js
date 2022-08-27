07c31c746587fe2d18e52554ea4222ad594a2327
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-27 08:20:22 +0530

    468. Organizing Multiple Tests with .describe()

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,14 +1,25 @@
-const expect = require('chai').expect
+const expect = require('chai').expect;
 
-const authMiddleware = require('../middleware/isAuth');
+const authMiddleware = require('../middleware/is-auth');
 
-it('Should throw an error if no authorization header is present', function() {
+describe('Auth middleware', function() {
+  it('should throw an error if no authorization header is present', function() {
     const req = {
-        get: function(headerName) {
-            return null;
-        }
-    }
-    //Passing a prepared (which functions and args passed in) reference
-    // authMiddleware(req, res, next)
-    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Not Authenticated');
-})
+      get: function(headerName) {
+        return null;
+      }
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      'Not authenticated.'
+    );
+  });
+
+  it('should throw an error if the authorization header is only one string', function() {
+    const req = {
+      get: function(headerName) {
+        return 'xyz';
+      }
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+});
