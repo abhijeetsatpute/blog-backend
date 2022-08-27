d4fa78ba7f382fcebaa7ad97a8f2ea7944a53694
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-27 08:43:24 +0530

    470. Using Stubs to modify external dependency and restore back when don

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,25 +1,46 @@
-const expect = require('chai').expect;
+const expect = require("chai").expect;
+const jwt = require('jsonwebtoken');
+const sinon = require('sinon');
 
-const authMiddleware = require('../middleware/is-auth');
+const authMiddleware = require("../middleware/isAuth");
 
-describe('Auth middleware', function() {
-  it('should throw an error if no authorization header is present', function() {
-    const req = {
-      get: function(headerName) {
-        return null;
-      }
-    };
-    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
-      'Not authenticated.'
-    );
-  });
+describe("Auth middleware", function () {
+    it("should throw an error if no authorization header is present", function () {
+        const req = {
+        get: function (headerName) {
+            return null;
+        },
+        };
+        expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+        "Not Authenticated"
+        );
+    });
 
-  it('should throw an error if the authorization header is only one string', function() {
-    const req = {
-      get: function(headerName) {
-        return 'xyz';
-      }
-    };
-    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
-  });
+    it('should yield a userId after decoding the token', function() {
+        const req = {
+        get: function(headerName) {
+            return 'Bearer djfkalsdjfaslfjdlas';
+        }
+        };
+        // stub (Object, MethodOfObj)
+        sinon.stub(jwt, 'verify');
+            // This stub also registers function calls
+        jwt.verify.returns({ userId: 'abc' });
+        authMiddleware(req, {}, () => {});
+        expect(req).to.have.property('userId');
+        expect(req).to.have.property('userId', 'abc');
+        expect(jwt.verify.called).to.be.true;
+        // This Restores the orignal function
+        jwt.verify.restore();
+    });
+
+    it("should throw an error if the authorization header is only one string", function () {
+        const req = {
+        get: function (headerName) {
+            return "xyz";
+        },
+        };
+        expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+    });
+
 });
